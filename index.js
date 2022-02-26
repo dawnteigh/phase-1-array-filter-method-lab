51fdbc5612338701d804d92efd02be4ab1bd96e0
Donte Ladatto <donte279@gmail.com>
2022-02-26 13:50:27 -0600

    commit tree 47fd8c6ad3606484ccd8dcb2f71d6dfd9564155b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+    let names = array.filter(obj => obj.toLowerCase() === string.toLowerCase())
+    if (names) {
+        return names;
+    }
+}
+function fuzzyMatch(array, string) {
+    let names = array.filter(obj => obj.startsWith(string))
+    if (names) {
+        return names;
+    }
+}
+function matchName(array, string) {
+    let names = array.filter(obj => obj.name === string)
+    if (names) {
+        return names;
+    }
+}
